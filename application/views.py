d5219baa0bf4eeedc885214011d90889ba3cc6dd
Sharbel Dahlan <sharbel.dahlan@gmail.com>
2020-02-17 21:15:31 +0200

    Update application index view to use DRF Response and make test pass

--- application/views.py
+++ application/views.py
@@ -1,2 +1,8 @@
-def index():
-    pass
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view()
+def index(request):
+    return Response({'message': 'index api view :)'}, status=status.HTTP_200_OK)
