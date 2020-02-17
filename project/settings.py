278a481b750deb9615a7b7ffdbf3fdd6ff3ed3e6
Sharbel Dahlan <sharbel.dahlan@gmail.com>
2020-02-17 20:43:48 +0200

    Add application and rest_framework to the settings' INSTALLED_APPS

--- project/settings.py
+++ project/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'application',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
