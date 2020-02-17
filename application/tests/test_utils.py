70486e3f3a6f9a73dbc2fa7cea64af967963a14c
Sharbel Dahlan <sharbel.dahlan@gmail.com>
2020-02-18 00:42:03 +0200

    Add more extreme cases to test_format_time()

--- application/tests/test_utils.py
+++ application/tests/test_utils.py
@@ -9,7 +9,10 @@ from application.utils import format_time
         (32400, '9 AM'),
         (37800, '10:30 AM'),
         (86399, '11:59:59 PM'),
-        (72000, '8 PM')
+        (72000, '8 PM'),
+        (32700, '9:05 AM'),
+        (32405, '9:00:05 AM'),
+        (0, '12 AM')
     ]
 )
 def test_format_time(unix_timestamp, formatted_time):
