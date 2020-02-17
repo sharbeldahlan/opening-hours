814712b22c1f251e5fa5bf051ef2439ec8f0ffd2
Sharbel Dahlan <sharbel.dahlan@gmail.com>
2020-02-17 21:15:13 +0200

    Update test case to use django rest framework's APIRequestFactory

--- application/tests/tests.py
+++ application/tests/tests.py
@@ -1,2 +1,13 @@
-def test_application_tests_can_run():
-    assert True
+from django.urls import reverse
+from rest_framework.test import APIRequestFactory
+
+from application.views import index
+
+
+def test_application_index__returns_200_and_correct_data():
+    url = reverse('index')
+    factory = APIRequestFactory()
+    request = factory.get(url)
+    response = index(request)
+    assert response.status_code == 200
+    assert response.data['message'] == 'index api view :)'
