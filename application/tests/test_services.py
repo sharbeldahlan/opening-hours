49b0be10e2c2c0e51fa7d641327074fdce924875
Sharbel Dahlan <sharbel.dahlan@gmail.com>
2020-02-18 03:41:22 +0200

    Update test that alters fixture data to not affect other tests

--- application/tests/test_services.py
+++ application/tests/test_services.py
@@ -84,15 +84,16 @@ def test_overflowing_closing_time():
     Test case for when a restaurant does not close during the same day,
     the overflowing closing time needs to appear on the same day of the opening time.
     """
-    data['monday'] = [{
+    new_data = data.copy()
+    new_data['monday'] = [{
         'type': 'close',
         'value': 95400
     }]
-    data['sunday'].append({
+    new_data['sunday'].append({
         'type': 'open',
         'value': 81000
     })
-    parsed_times_dict = parse_input_times(data)
+    parsed_times_dict = parse_input_times(new_data)
     assert parsed_times_dict['sunday'] == ['12 PM - 9 PM', '10:30 PM - 2:30 AM']
 
 
